d07b3b68b6a306016e7f6254e7e774658c25fd35
Joann Holland <>
2018-10-03 18:18:11 +0100

    Added posts page and css file.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // npm install express, ejs --save
+// sudo npm install nodemon --save -g
 
 var express = require("express");
 var app = express();
+
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
   res.render("home.ejs");
@@ -10,6 +13,15 @@ app.get("/", function(req, res){
 app.get("/fallinlovewith/:thing", function(req,res){
   var thing = req.params.thing;
   res.render("love.ejs", {thing: thing});
+});
+
+app.get("/posts", function(req, res){
+  var posts = [
+    {title: "Title 1", author: "Author 1"},
+    {title: "Title 2", author: "Author 2"},
+    {title: "Title 3", author: "Author 3"}
+  ];
+  res.render("posts.ejs", {posts: posts});
 });
 
 app.listen(3000, function(){
