343c19b5aed7161a598aa3186aa801b54a0552dc
Joann Holland <>
2018-10-03 17:30:06 +0100

    Added love.ejs file demonstrating how to put information from the path into the file.

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+// npm install express, ejs --save
+
 var express = require("express");
 var app = express();
 
 app.get("/", function(req, res){
   res.render("home.ejs");
+});
+
+app.get("/fallinlovewith/:thing", function(req,res){
+  var thing = req.params.thing;
+  res.render("love.ejs", {thing: thing});
 });
 
 app.listen(3000, function(){
