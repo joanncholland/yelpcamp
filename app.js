ce449f6021bf991654c6c4670745045473527853
Joann Holland <joann.c.holland@btinternet.com>
2018-10-04 12:52:04 +0100

    Fixed spelling error

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get('/', function(req,res){
 app.get('/campgrounds', function(req,res){
   var campgrounds = [
     {name: 'Salmon Creek', image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104496f8c470aee9bdb9_340.jpg"},
-    {name: 'Granite Holls', image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104496f8c470aee9bdb9_340.jpg"},
+    {name: 'Granite Hills', image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104496f8c470aee9bdb9_340.jpg"},
     {name: "Mountain Goats' Rest", image: "https://farm1.staticflickr.com/82/225912054_690e32830d.jpg"}
   ];
 
